18206be1e120acd8d301a6a08ce0c646387ee2a4
zhangxiahui <281055769@qq.com>
2017-09-11 18:18:43 +0800

    #综合信息平台 补签功能 1.2.16

--- src/main/java/com/trasen/imis/service/WeixinService.java
+++ src/main/java/com/trasen/imis/service/WeixinService.java
@@ -268,12 +268,14 @@ public class WeixinService {
                 }
 
 
+                long v = new Date().getTime();
 
                 if (content.contains("公司考勤")) {
                     String attenceUrl = PropertiesUtils.getProperty("attence_url");
                     if(attenceUrl==null){
                         attenceUrl = "http://bpmtest01.trasen.cn/src/index.html#/mobile";
                     }
+                    attenceUrl =attenceUrl +"?v="+v;
                     TextMessage tm = new TextMessage();
                     tm.setToUserName(FromUserName);
                     tm.setFromUserName(ToUserName);
@@ -288,7 +290,7 @@ public class WeixinService {
                     if(attenceUrl==null){
                         attenceUrl = "http://bpmtest01.trasen.cn/src/index.html#/mobile";
                     }
-                    attenceUrl = attenceUrl+"?attType=1";
+                    attenceUrl = attenceUrl+"?attType=1&v="+v;
                     TextMessage tm = new TextMessage();
                     tm.setToUserName(FromUserName);
                     tm.setFromUserName(ToUserName);
